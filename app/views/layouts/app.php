f40cd16aba7bc69fa2a5167956bc51f881b595cf
Naomikoshi <lingsuling276@gmail.com>
2026-04-10 10:55:51 +0700

    feat: implement layout

--- app/views/layouts/app.php
+++ app/views/layouts/app.php
@@ -7,20 +7,12 @@
     <link rel="stylesheet" href="/css/output.css">
 </head>
 <body class="min-h-screen flex flex-col bg-gray-100">
-    <header class="bg-blue-500 text-white">
-        <div class="flex items-center justify-between container mx-auto p-[16px]">
-            <a href="/students" class="font-bold text-xl">Sistem Sekolah</a>
-            <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
-        </div>
-    </header>
+    
+    <?php require_once '../app/views/layouts/partials/header.php'?>
 
     <main class="grow container mx-auto">
         <?php require_once $content ?>
     </main>
-    <footer class="bg-gray-800 text-white">
-        <div class="text-center p-4">
-            &copy <?= date('Y')?> - Sistem Sekolah SMK Kristen Immanuel
-        </div>
-    </footer>
+    <?php require_once '../app/views/layouts/partials/footer.php'?>
 </body>
 </html>
--- app/views/students/edit.php
+++ app/views/students/edit.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Siswa</title>
-    <link rel="stylesheet" href="/css/output.css">
-</head>
-<body class="min-h-screen flex flex-col bg-gray-100">
-    <header class="bg-blue-500 text-white">
-        <div class="flex items-center justify-between container mx-auto p-[16px]">
-            <a href="/students" class="font-bold text-xl">Sistem Sekolah</a>
-            <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
-        </div>
-    </header>
-
-    <main class="grow container mx-auto">
-        <div class="mt-8 space-y-4">
+<div class="mt-8 space-y-4">
             <div class="bg-white shadow rounded-lg p-4">
                 <h1 class="text-2xl font-bold">Edit Siswa</h1>   
                 <p>Melakukan perubahan data siswa yang terdaftar</p>
@@ -46,11 +29,3 @@
                 </form>
             </div>
         </div>
-    </main>
-    <footer class="bg-gray-800 text-white">
-        <div class="text-center p-4">
-            &copy <?= date('Y')?> - Sistem Sekolah SMK Kristen Immanuel
-        </div>
-    </footer>
-</body>
-</html>
